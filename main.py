f17702277968f1ef563c3290367324cabf412818
trevor <trevor231995@gmail.com>
2026-02-09 16:28:44 +0800

    initial

--- main.py
+++ main.py
@@ -70,10 +70,10 @@ if __name__ == '__main__':
     ######################################################################Phase 2 Obtain Empirical Distribution #############################################################################################3
     ##############################################################################################################################################################################################
     #2.1 PoI Selection
-    poi_xors = poi_selection_options(dataset, nb_poi, total_samplept, number_of_traces, X_profiling, L_profiling, image_root,poi_root, poi_selection_mode, plot_cpa_image=True, save_poi=True)
+    poi_xors = poi_selection_options(dataset, nb_poi, total_samplept, number_of_traces, X_profiling, L_profiling, image_root,poi_root, poi_selection_mode, plot_cpa_image=True, save_poi=False)
 
     #2.2 Labeling Training traces.
-    Y_train_solo_all_hw = labeling_traces(X_profiling, poi_xors, num_bits, save_root, labeling_type, poi_selection_mode, dataset, num_branch, save_labels=True)
+    Y_train_solo_all_hw = labeling_traces(X_profiling, poi_xors, num_bits, save_root, labeling_type, poi_selection_mode, dataset, num_branch, save_labels=False)
     print("Y_train_solo_all_hw:", Y_train_solo_all_hw, Y_train_solo_all_hw.shape)  # [nb_traces, 2]
     Y_train_combined_hws = deepcopy(Y_train_solo_all_hw[:, 0])
     for i in range(1, Y_train_solo_all_hw.shape[1]):
